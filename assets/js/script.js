cbe3b863db6f23f0fda0c700f0ff81078e1bcb15
LFG115 <>
2023-11-05 19:26:12 +0000

    add rules and img to give information on the game

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+function gameTallyBoard() {
+
+    let userText = document.getElementById("userText").innerText;
+    let cpuText = document.getElementById("cpuText").innerText;
+    let resultText = document.getElementById("resultText").innerText;
+}
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectScore() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
